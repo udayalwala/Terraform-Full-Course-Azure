d1fe467bc130784adff29d8bf6eb4f633a254d1b
udayalwala <55484560+udayalwala@users.noreply.github.com>
2025-05-17 00:55:09 +0530

    Create var1.tf

---
+++ lessons/day05/var1.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.29.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "tfstatefilesrg"
+      storage_account_name = "tfstatefilesc"
+      container_name       = "tfstate"
+      key                  = "demo.terraform.tfstate"
+  }
+  required_version = ">= 1.12.0"
+}
+
+provider "azurerm" {
+  features {}
+
+}
+variable "environment" {
+  type = string
+  description = "environment stage"
+  default = "staging"
+  
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "azurelearningsbyudai"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = var.environment
+  }
+}
